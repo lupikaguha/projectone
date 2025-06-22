5dbaf71c15322f5a8e3ccba953fc4a584793c9a4
lupikaguha <lupikaguha@gmail.com>
2025-06-22 09:54:16 +0530

    UPDATE

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Badges } from "./Badges";
+import { Breadcrumbs } from "./Breadcrumbs";
 function App() {
   return (
     <div>
       <Header />
       <div>Home</div>
       <Footer />
+      <Badges></Badges>
+      <Breadcrumbs></Breadcrumbs>
     </div>
   );
 }
-
-export default App;
